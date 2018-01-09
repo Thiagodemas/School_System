7b4dce3d0ef0b927a6962d224ac5783a0357edea
thiagodemas <thiago.demas7@gmail.com>
2018-01-09 18:25:32 -0300

    Shool entrance

--- entrance.php
+++ entrance.php
@@ -1,5 +1,4 @@
 <?php
-
 $now_category = "Entrance";
 ?>
 <!DOCTYPE html>
@@ -11,12 +10,45 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+        <title>School Entrance</title>
+
+        <link rel="stylesheet" type="text/css" href="css/entrance.css"/>
+
         <?php
         require_once 'authentication_config.php';
         ?>
-        
+
+    </head>
+    <body>
+        <div id="box">
+            <div id="entrance">
+                <h1><strong>Your code is:</strong><a href="authentication_config.php?action=and"><strong>Logout</strong></a></h1>
+            </div>
+            <div id="logo">
+                <img src="img/logo.svg" width="250px"/>
+            </div>
+            <?php
+            if (isset($_POST['send'])) {
+                $cpf = mysqli_real_escape_string($mysqli, $_POST['cpf']);
+
+                if (empty($code)) {
+                    echo '<h2>Please, enter the card number or access code!</h2>';
+                }
+            }
+            ?>
+
+            <div id="searchField">
+                <form  method="post" action="entrance.php" name="send" enctype="multipart/form-data">
+                    <input type="text" name="cpf" value=""/>
+                    <input class="input" type="submit" name="submit" value="Search"/>
+                </form>
+
+
+                <br><br><br><br><h3><strong>Student:</strong><strong>Registration Number:</strong><strong>RG:</strong>
+                    <a href="index.php?pg=confirma&code_a=<img src='img/corret.png' title='Confirm' border='0'"/></a>
+                    <a href="index.php"><img src="img/delete.png" width="24px" title="Cancelar"/></a></h3><input type="hidden" name="codes" value=""/>
+            </div>
+            <br><br><br>
+        </div>
     </body>
 </html>
